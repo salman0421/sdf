771e20b8b8c11d2e82d6130978cb1aab2272bf66
QAP10 <QAP10@QAP10-PC>
2019-04-08 18:34:56 +0530

    sdr

---
+++ Selenium/src/operators/Increment_decrement.java
@@ -0,0 +1,18 @@
+package operators;
+
+public class Increment_decrement {
+
+	public static void main(String[] args) {
+	Object a[]= new Object[5];
+	a[0]=100;
+	a[1]="ranaprathap";
+	a[2]=true;
+	a[3]=false;
+	a[4]=100.12;
+	
+	System.out.println(a[3]);
+	int rana = a.length;
+	System.out.println(rana);
+	}
+
+}
